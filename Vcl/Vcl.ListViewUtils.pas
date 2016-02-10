ccaf1198ab66bf79e6b56e24684fa2742ab79f1d
Agustin Ortu <ortu.agustin@gmail.com>
2016-02-09 22:38:46 -0300

    Typo

--- Vcl/Vcl.ListViewUtils.pas
+++ Vcl/Vcl.ListViewUtils.pas
@@ -5,17 +5,30 @@ interface
 // source: http://delphiaccess.com/foros/index.php/topic/11740-como-colocar-las-flechitas-que-indican-el-orden-de-columnas-de-un-listview/
 // author: escafandra
 procedure SetSortIcon(const AListViewHandle: NativeInt; const ColIndex: Integer; const Ascending: Boolean);
+procedure ClearSortIcon(const AListViewHandle: NativeInt; const ColIndex: Integer);
 
 implementation
 
 uses
   {$IF CompilerVersion > 21}
-  Vcl.CommCtrl,
+  Winapi.CommCtrl,
   Winapi.Windows;
   {$ELSE}
   CommCtrl,
   Windows;
   {$IFEND}
+
+procedure ClearSortIcon(const AListViewHandle: NativeInt; const ColIndex: Integer);
+var
+  LHeader: THandle;
+  LItem: HD_ITEM;
+begin
+  LHeader := SendMessage(AListViewHandle, LVM_GETHEADER, 0, 0);
+  LItem.Mask := HDI_FORMAT;
+  SendMessage(LHeader, HDM_GETITEM, ColIndex, Integer(@LItem));
+  LItem.fmt := (LItem.fmt and not(HDF_SORTUP or HDF_SORTDOWN));
+  SendMessage(LHeader, HDM_SETITEM, ColIndex, Integer(@LItem));
+end;
 
 procedure SetSortIcon(const AListViewHandle: NativeInt; const ColIndex: Integer; const Ascending: Boolean);
 const
